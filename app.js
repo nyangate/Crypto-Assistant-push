ef752055d6007c3a105b73491bf9bd0e894dc4d5
robertny <robertny@ondemand.co.ke>
2017-08-15 22:43:37 +0300

    set 2hr expiry for saved limits

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var morgan =require('morgan');
 var dateFormat = require('dateformat');
 var schedule = require('node-schedule');
 var request = require('request');
+var redis = require("redis");
+var moment = require('moment');
 app.use(morgan('dev'));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded());
@@ -15,22 +17,48 @@ admin.initializeApp({
   credential: admin.credential.cert(cserviceAccount),
   databaseURL: "https://crypto-assistant.firebaseio.com/"
 });
+var client = redis.createClient({
+    retry_strategy: function (options) {
+        if (options.error && options.error.code === 'ECONNREFUSED') {
+            // End reconnecting on a specific error and flush all commands with a individual error
+            return new Error('The server refused the connection');
+        }
+        if (options.total_retry_time > 1000 * 60 * 60) {
+            // End reconnecting after a specific timeout and flush all commands with a individual error
+            return new Error('Retry time exhausted');
+        }
+        if (options.attempt > 10) {
+            // End reconnecting with built in error
+            return undefined;
+        }
+        // reconnect after
+        return Math.min(options.attempt * 100, 3000);
+    }
+});
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
 
 var defaultAuth = admin.auth();
 var customerdb = admin.database();
-var customerootRef = customerdb.ref();
 var usersRef = customerdb.ref("users");
+fetchUpdates();
 
 
 
 
+function sendAlert(order,priceChange,increase,limit){
+  var key = ("messages/"+order.id+"_"+limit+"_"+increase);
+  client.get(key, function(err, reply) {
+    if(reply){
+          console.log("Message has already been sent");
 
-function sendAlert(order,priceChange,increase){
+      }else{
       var payload = {
 
         data: {
             orderId: order.id,
-            title: "Bitcoin price is now $"+priceChange,
+            title: "Bitcoin price: $"+priceChange,
             body: increase?"Click to find out why price is increasing":"Click to find out why price is decreasing"
           }
         };
@@ -40,10 +68,20 @@ function sendAlert(order,priceChange,increase){
               // the contents of response.
 
               console.log("Successfully sent message:", response);
+              client.set(key,priceChange)
+              // var expiryDate = new LocalDate().plusHours(1);
+              var expiryDate = moment().add(120, 'minutes').toDate();
+              console.log(expiryDate);
+              client.expireat(key, parseInt(expiryDate/1000));
             })
             .catch(function(error) {
               console.log("Error sending message:", error);
             });
+          }
+
+
+  });
+
 
     }
 function getUsers(priceChange){
@@ -51,116 +89,18 @@ function getUsers(priceChange){
   snapshot.forEach(function(data) {
     var greaterthan = JSON.parse(data.val()).greaterThan;
     var lessthan = JSON.parse(data.val()).lessThan;
-    console.log("The user id " + data.key + "  has greater than " + greaterthan+" and less than "+lessthan);
     if(priceChange>=greaterthan){
-      sendAlert(JSON.parse(data.val()),priceChange,true);
+      sendAlert(JSON.parse(data.val()),priceChange,true,greaterthan);
     }
     if(priceChange<lessthan){
-      sendAlert(JSON.parse(data.val()),priceChange,false);
+      sendAlert(JSON.parse(data.val()),priceChange,false,lessthan);
     }
   });
   });
 
 
 }
-function scheduleOrder(order){
-    try{
-      var date = new Date(Date.parse(order.scheduled));
-        console.log("scheduling order at "+date);
-        var j = schedule.scheduleJob(date, function(){
-            console.log('Scheduled order is being executed now');
-            alertDrivers(order);
-          });
-    }catch(e){
-      console.log(e);
-      alertDrivers(order);
-    }
 
-}
-
-function rejectOrder(key){
-
-  var query = drootRef.child("orders");
-  query.child(key).once("value",function(snap){
-    if(snap.val()){
-        console.log("rebroadcasting "+key);
-      var childData = snap.val();
-      var order = JSON.parse(childData);
-        deliveriesRef.child(key).set(childData);
-        alertDrivers(order);
-    }
-
-
-
-  });
-
-
-}
-
-function listenToDriverResponses(){
-  var dquery = pickedOrdersRef.orderByKey();
-  dquery.on("child_added", function(childSnapshot, prevChildKey) {
-    var key = childSnapshot.key;
-    console.log(key+ " has been picked");
-    deliveriesRef.child(key).remove();
-    pickedOrdersRef.child(key).remove();
-    console.log("sending picked alert to customer");
-    alertCustomer(key);
-
-    trackOrder(key);
-  });
-
-}
-
-function trackOrder(key){
-  drootRef.child("orders").child(key).on("value",function(snap){
-    if(!snap.val())
-    return;
-    var childData = snap.val();
-    var order = JSON.parse(childData);
-    console.log(key+ " order "+key+" is at "+order.driverlocation);
-
-      customerootRef.child("ontrack").child(order.orderId).child("driverlocation").set(""+order.driverlocation);
-      customerootRef.child("ontrack").child(order.orderId).child("driverphone").set(order.driverphone);
-      customerootRef.child("ontrack").child(order.orderId).child("maplocation").set(""+order.maplocation);
-      customerootRef.child("ontrack").child(order.orderId).child("driverid").set(order.driverid);
-      customerootRef.child("ontrack").child(order.orderId).child("orderId").set(order.orderId);
-      customerootRef.child("ontrack").child(order.orderId).child("status").set(order.status);
-      customerootRef.child("ontrack").child(order.orderId).child("location").set(order.location);
-      if(order.delivered)
-      customerootRef.child("ontrack").child(order.orderId).child("delivered").set(order.delivered);
-
-  },function (errorObject) {
-    console.log("The read failed: " + errorObject.code);
-  });
-}
-
-function alertCustomer(key){
-  drootRef.child("orders").child(key).once("value",function(snap){
-    var childData = snap.val();
-    var order = JSON.parse(childData);
-    var payload = {
-
-      data: {
-        orderId: order.orderId,
-        location: order.location,
-        title: "Driver Response alert",
-        body: "A driver has responded to pick your order. You can track your order now!",
-        phone: order.phone
-      }
-    };
-    admin.messaging().sendToDevice(order.token, payload)
-      .then(function(response) {
-        // See the MessagingDevicesResponse reference documentation for
-        // the contents of response.
-
-        console.log("Successfully sent message:", response);
-      })
-      .catch(function(error) {
-        console.log("Error sending message:", error);
-      });
-  });
-}
 function fetchUpdates(){
   setInterval(function() {
     request.get("https://blockchain.info/ticker",
@@ -169,15 +109,15 @@ function fetchUpdates(){
         console.log(err);
         return;
          }
-      //console.log(body);
-      getUsers(4117);
+      console.log("USD/BTC now at: "+JSON.parse(body).USD.buy);
+      getUsers(JSON.parse(body).USD.buy);
 
     });
   }, 5000);
 
 }
-fetchUpdates();
+
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('app is listening on port 3000')
 })
